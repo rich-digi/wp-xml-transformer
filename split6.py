47937054e06b9fa48043e67789514666eaf47f31
Richard Knight <rk@Richards-MacBook-Pro-557.local>
2014-11-06 17:01:43 +0000

    XML is painful

--- split6.py
+++ split6.py
@@ -2,10 +2,11 @@
 # Split Wordpress XML (using Element Tree)
 # --------------------------------------------------------------------------------
 
-import sys
+import sys, os, re
 sys.path.append('/usr/local/lib/python2.7/site-packages/')
 
 from lxml import etree as ET
+import codecs
 
 xmldata = 'input/dmclubcustomerblog.wordpress.2014-10-29.xml'
 
@@ -23,24 +24,41 @@ for prefix, uri in namespaces.iteritems():
 
 # Now parse the XML
 i = 0;
-for event, elem in ET.iterparse(xmldata, ['start', 'end', 'start-ns', 'end-ns'], strip_cdata=False, remove_blank_text=True):
+for event, elem in ET.iterparse(xmldata, ['start', 'end', 'start-ns', 'end-ns'], encoding='utf-8', strip_cdata=False, remove_blank_text=True):
 
 	if event == 'start':
 		# Remove XML namespace prefix	
 		tag = ET.QName(elem.tag).localname
 		
 		if tag == 'item':
-			title = elem.find('title').text
-			content = elem.find('content:encoded').text
-			#elem.remove(content)
-		
-			print
 			print '-------------------------------------------------------------------------------'
-			print 'ITEM', title
+			title = elem.find('title').text
+			print title
+			type = elem.find('wp:post_type', namespaces=namespaces)
+			print type
+			if type == None:
+				type = elem.xpath('wp:post_type', namespaces=namespaces)
+				print type
 			
-			xmlstr = ET.tostring(elem, pretty_print=True, encoding='utf8', method='xml')
+			content = elem.find('content:encoded', namespaces=namespaces)
+			elem.remove(content)
 		
-			f = open('output/post_'+str(i)+'.xml', 'w');
+			print type, title
+			print
+			
+			xmlstr = ET.tostring(elem, pretty_print=True, encoding='unicode', method='xml')
+			
+			dir = ''.join([os.getcwd()+'/output/post__', re.sub(r'[^\w]', '_', title.lower())])
+			print dir
+			
+			if not os.path.exists(dir): os.makedirs(dir)
+
+			f = codecs.open(dir+'/meta.xml', 'w', 'utf-8');
 			f.write(xmlstr)
 			f.close()
+			
+			f = codecs.open(dir+'/contnet.html', 'w', 'utf-8');
+			f.write(content.text)
+			f.close()
+
 			i += 1
